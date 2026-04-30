f7e599c51a01e88b689b12de0cd41f0fd4959f2d
Connor Slade <connor@connorcode.com>
2026-04-30 00:37:23 -0400

    WiFi OTA Updates

--- embeded_update/src/main.rs
+++ embeded_update/src/main.rs
@@ -1,21 +1,25 @@
-use std::{mem::ManuallyDrop, sync::Arc};
+use std::{mem::ManuallyDrop, thread};
 
 use anyhow::{Error, Result};
-use esp_idf_hal::{delay::FreeRtos, modem::WifiModem};
+use esp_idf_hal::{delay::FreeRtos, peripherals::Peripherals};
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
     http::{Method, server::EspHttpServer},
-    mdns::EspMdns,
+    log::EspLogger,
+    nvs::EspDefaultNvsPartition,
     ota::EspOta,
     wifi::{AccessPointConfiguration, AuthMethod, BlockingWifi, Configuration, EspWifi},
 };
-use log::info;
 
-use crate::app::App;
+fn main() -> Result<()> {
+    esp_idf_svc::sys::link_patches();
+    EspLogger::initialize_default();
 
-pub fn init(app: Arc<App>, modem: WifiModem<'static>) -> Result<()> {
+    let peripherals = Peripherals::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
     let sysloop = EspSystemEventLoop::take()?;
-    let driver = EspWifi::new(modem, sysloop.clone(), Some(app.nvs.clone()))?;
+    let driver = EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs))?;
     let mut wifi = ManuallyDrop::new(BlockingWifi::wrap(driver, sysloop)?);
 
     let config = AccessPointConfiguration {
@@ -28,10 +32,6 @@ pub fn init(app: Arc<App>, modem: WifiModem<'static>) -> Result<()> {
     wifi.set_configuration(&Configuration::AccessPoint(config))?;
     wifi.start()?;
     wifi.wait_netif_up()?;
-    info!("Initialized WiFi");
-
-    let ip_info = wifi.wifi().ap_netif().get_ip_info()?;
-    info!("AP: {:?}", ip_info.ip);
 
     let mut http = ManuallyDrop::new(EspHttpServer::new(&Default::default())?);
     http.fn_handler::<Error, _>("/", Method::Get, |req| {
@@ -40,18 +40,32 @@ pub fn init(app: Arc<App>, modem: WifiModem<'static>) -> Result<()> {
         Ok(())
     })?;
 
-    http.fn_handler::<Error, _>("/update", Method::Post, |req| {
+    http.fn_handler::<Error, _>("/flash", Method::Post, |mut req| {
         let mut ota = EspOta::new()?;
-        ota.factory_reset()?;
+        let mut update = ota.initiate_update()?;
+
+        let mut buffer = [0; 512];
+        loop {
+            let size = req.read(&mut buffer)?;
+            if size == 0 {
+                break;
+            }
+
+            update.write(&buffer[..size])?;
+        }
+
+        update.complete()?;
         req.into_ok_response()?.flush()?;
 
         FreeRtos::delay_ms(500);
         esp_idf_hal::reset::restart();
     })?;
 
-    let mut mdns = ManuallyDrop::new(EspMdns::take()?);
-    mdns.set_hostname("windlink")?;
-    mdns.add_service(None, "_http", "_tcp", 80, &[])?;
+    // let mut mdns = ManuallyDrop::new(EspMdns::take()?);
+    // mdns.set_hostname("windlink")?;
+    // mdns.add_service(None, "_http", "_tcp", 80, &[])?;
 
-    Ok(())
+    loop {
+        thread::park();
+    }
 }
